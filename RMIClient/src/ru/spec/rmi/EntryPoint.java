b27d56a27365f073eb64ae1e4ecd7206c7ad62ca
User <User@tg_-__>
2017-04-18 21:41:07 +0300

    init
    RMI КЛИЕНТ И СЕРВЕР

---
+++ RMIClient/src/ru/spec/rmi/EntryPoint.java
@@ -0,0 +1,22 @@
+package ru.spec.rmi;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import ru.spec.ee.ejb.IEchoService;
+
+public class EntryPoint {
+
+	public static void main(String[] args) throws NamingException {
+		Context ctx = new InitialContext();
+
+		IEchoService echoBean = 
+				(IEchoService)ctx.lookup("echoBean");
+		
+		String result = echoBean.echo("hello");
+		System.out.println(result);
+
+	}
+
+}
